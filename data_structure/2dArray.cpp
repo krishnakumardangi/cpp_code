bbaae6b0a5009584e287ffa88868b26e633e8fa4
Krishna <studyroom19471@gmail.com>
2023-12-13 22:41:13 +0530

    Basic 2D Array Code

---
+++ data_structure/2dArray.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std; 
+
+int main( ) 
+{
+	int  s[3][3];
+	int  i, j;
+	cout<<"\n2D Array Input:\n";
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			cout<<"\ns["<<i<<"]["<<j<<"]=  ";
+			cin>>s[i][j];
+		}
+	}
+	
+	cout<<"\nThe 2-D Array is:\n";
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			cout<<"\t"<<s[i][j];
+		}
+		cout<<endl;
+	}
+	return 0; 
+}
